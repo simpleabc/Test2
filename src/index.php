f41dd4985f5939e9f3bd657ffd2943f5804a6572
simpleabc <774879651@qq.com>
2015-11-19 10:09:52 +0800

    我的PHP项目

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<!--
+   Script name  : Ajax Auto Suggest
+   File Name 	: Index.php
+   Developed By : Amit Patil (India)
+   Email Id 	: amitpatil321@gmail.com
+   last Updated : 9 June 2009
+         This program is freeware.There is no any fucking copyright and bla bla bla.
+   You can use it for your personal use.You can also make any changes to this script.
+   But before using this script i would appericiate your mail.That will encourage me a lot.
+   Any suggestions are always welcome.
+         Have a fun with programming.   
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE> Ajax Auto Suggest </TITLE>
+  <META NAME="Generator" CONTENT="EditPlus">
+  <META NAME="Author" CONTENT="">
+  <META NAME="Keywords" CONTENT="">
+  <META NAME="Description" CONTENT="">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <!--[if gte IE 6]>
+    <link rel="stylesheet" type="text/css" href="i_hate_IE.css" />
+  <![endif]-->
+ <SCRIPT LANGUAGE="JavaScript" src="js/jquery.js"></SCRIPT>
+ <SCRIPT LANGUAGE="JavaScript" src="js/script.js"></SCRIPT>
+ </HEAD>
+ <BODY>
+ <center>
+   <div class="main">
+     	 <div id="holder"> 
+		 Enter Keyword : <input type="text" id="keyword" tabindex="0"><img src="images/loading.gif" id="loading">
+		 </div>
+		 <div id="ajax_response"></div>
+	 <div class="about"><a href="javascript:void(0);">Program By : Amit Patil (amitpatil321@gmail.com)</a></span></div>
+   </div>
+ </center>
+ </BODY>
+</HTML>
